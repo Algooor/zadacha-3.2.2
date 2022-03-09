8f71f5fb374011f4670b9c5956e3d2ffca7ee71b
algor <in.glebov@bk.ru>
2022-03-09 09:28:33 +0700

    доработка корректности отображения если последние цифры количкества лайков "11"

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,7 @@
 fun main() {
-    val likes = 314
+    val likes = 173489781
     val lastNumber = likes%10
-    if (lastNumber == 1) println("Понравилось $likes человеку") else println("Понравилось $likes людям")
+    val lastNumber11 = likes%100
+    if (lastNumber11 == 11) println("Понравилось $likes людям") else if (lastNumber == 1) println("Понравилось $likes человеку") else println("Понравилось $likes людям")
+
 }
